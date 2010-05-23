e05c656e9ba2d4f32cd384946588dc339c6cbafa
deyan <deyan@2d0c7756-e317-0410-b71c-cbb6040a58f7>
2010-05-23 09:09:35 +0000

    adding support for URI resolving and fixes to file passing through sockets
    git-svn-id: https://svn.mathweb.org/repos/LaTeXML@1155 2d0c7756-e317-0410-b71c-cbb6040a58f7

--- branches/arXMLiv/lib/LaTeXML/Daemon.pm
+++ branches/arXMLiv/lib/LaTeXML/Daemon.pm
@@ -22,6 +22,7 @@ use LaTeXML::Object;
 use LaTeXML::MathParser;
 use LaTeXML::Util::Pathname;
 use LaTeXML::Bib;
+use LWP::Simple;
 use Encode;
 our @ISA = (qw(LaTeXML));
 
@@ -65,7 +66,7 @@ sub digestFileDaemonized {
 }
 
 sub digestBibTeXFileDaemonized {
-  my($self,$file)=@_;
+  my($self,$file,$mode)=@_;
   $file =~ s/\.bib$//;
   $self->withState(sub {
      my($state)=@_;
@@ -89,17 +90,62 @@ sub digestBibTeXFileDaemonized {
 }
 
 sub digestStringDaemonized {
-  my($self,$string,$sourcebase)=@_;
+  my($self,$string,$mode,$sourcebase)=@_;
   $self->withState(sub {
      my($state)=@_;
      NoteBegin("Digesting string");
      #We only need to initialize the state at the start of the daemon!
      #$self->initializeState('TeX.pool', @{$$self{preload} || []});
      $state->assignValue(SOURCEBASE=>$sourcebase,'global') if $sourcebase;
+     #Note that we first open the \end and then the \begin
+     #Since we have a stack and not a queue.
+     if ($mode eq "fragment" && $state->lookupDefinition(T_CS("\\begin{document}"))) {
+       #End {document}
+       my $edoc = '\\end{document}';
+       $state->getStomach->getGullet->openMouth(LaTeXML::Mouth->new($edoc),0);
+     }
+     #Digest input
      $state->getStomach->getGullet->openMouth(LaTeXML::Mouth->new($string),0);
-###     $state->installDefinition(LaTeXML::Expandable->new(T_CS('\jobname'),undef,
-###						       Tokens(Explode("Unknown"))));
+     #Wrap LaTeX fragments in a {document} environment
+     if ($mode eq "fragment" && $state->lookupDefinition(T_CS("\\begin{document}"))) {
+       my $bdoc = '\\begin{document}';
+       $state->getStomach->getGullet->openMouth(LaTeXML::Mouth->new($bdoc),0);
+     }
+
      my $line = $self->finishDigestion;
      NoteEnd("Digesting string");
+     $line; });
+}
+
+sub digestURLDaemonized {
+  my($self,$url,$mode)=@_;
+  #Web-based
+  die "Can't access source URL: $url\n" unless (head($url));
+  my $content = get($url);
+  die "Failed to fetch source URL: $url\n"  unless ($content);
+  $self->withState(sub {
+     my($state)=@_;
+     NoteBegin("Digesting URL: $url");
+     #We only need to initialize the state at the start of the daemon!
+     #$self->initializeState('TeX.pool', @{$$self{preload} || []});
+     $state->assignValue(SOURCEBASE=>$url,'global');
+     $state->assignValue(SOURCEFILE=>$url,'global');
+     #Note that we first open the \end and then the \begin
+     #Since we have a stack and not a queue.
+     if ($mode eq "fragment" && $state->lookupDefinition(T_CS("\\begin{document}"))) {
+       #End {document}
+       my $edoc = '\\end{document}';
+       $state->getStomach->getGullet->openMouth(LaTeXML::Mouth->new($edoc),0);
+     }
+     #Digest input
+     $state->getStomach->getGullet->openMouth(LaTeXML::Mouth->new($content),0);
+     #Wrap LaTeX fragments in a {document} environment
+     if ($mode eq "fragment" && $state->lookupDefinition(T_CS("\\begin{document}"))) {
+       my $bdoc = '\\begin{document}';
+       $state->getStomach->getGullet->openMouth(LaTeXML::Mouth->new($bdoc),0);
+     }
+     
+     my $line = $self->finishDigestion;
+     NoteEnd("Digesting URL: $url");
      $line; });
 }
