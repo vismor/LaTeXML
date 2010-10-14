3f0744329f001a8283da76feb79776dd141e7e8b
deyan <deyan@2d0c7756-e317-0410-b71c-cbb6040a58f7>
2010-10-14 16:34:12 +0000

    adding documentation to LaTeXML::Daemon module
    git-svn-id: https://svn.mathweb.org/repos/LaTeXML@1344 2d0c7756-e317-0410-b71c-cbb6040a58f7

--- branches/arXMLiv/lib/LaTeXML/Daemon.pm
+++ branches/arXMLiv/lib/LaTeXML/Daemon.pm
@@ -6,7 +6,7 @@
 # |  Public domain software, produced as part of work done by the       | #
 # |  United States Government & not subject to copyright in the US.     | #
 # |---------------------------------------------------------------------| #
-# | Bruce Miller <bruce.miller@nist.gov>                        #_#     | #
+# | Deyan Ginev <d.ginev@jacobs-university.de>                  #_#     | #
 # | http://dlmf.nist.gov/LaTeXML/                              (o o)    | #
 # \=========================================================ooo==U==ooo=/ #
 
@@ -149,3 +149,71 @@ sub digestURLDaemonized {
      NoteEnd("Digesting URL: $url");
      $line; });
 }
+
+__END__
+
+=pod 
+
+=head1 NAME
+
+C<LaTeXML::Daemon> - Daemonized digestion routines for the C<LaTeXML> class.
+
+=head1 SYNOPSIS
+
+    use LaTeXML::Daemon;
+    my $latexml = LaTeXML::Daemon->new(%options);
+    $latexml->digestURLDaemonized($url,$mode);
+    $latexml->digestBibTeXFileDaemonized($bibfile,$mode);
+    $latexml->digestFileDaemonized($file,$mode);
+    $latexml->digestStringDaemonized($string,$mode);
+
+Also see the utility daemon servers C<latexmls> and C<latexmld>, 
+which utilize this module.
+
+=head1 DESCRIPTION
+
+The C<LaTeXML::Daemon> class inherits from the main C<LaTeXML> class, 
+adding four daemonized digestion routines, essentially skipping the 
+initialization steps of the different C<LaTeXML::State> objects.
+
+=head2 METHODS
+
+=over 4
+
+=item C<< $box = $latexml->digestURLDaemonized($url,$mode); >>
+
+Fetches and digests a document from a given URL, skipping the 
+initialization steps of C<LaTeXML::digestURL>. 
+Adds {document} wrapper in fragment mode.
+Warning: Untested!
+
+=item C<< $box = $latexml->digestBibTeXFileDaemonized($bibfile,$mode); >>
+
+Digests a BibTeX file, skipping the initialization steps 
+of C<LaTeXML::digestBibTeXFile>
+Warning: Untested!
+
+=item C<< $box = $latexml->digestFileDaemonized($file,$mode); >>
+
+Digests a file, skipping the initialization steps 
+of C<LaTeXML::digestFile>
+Adds {document} wrapper in fragment mode.
+
+=item C<< $box = $latexml->digestStringDaemonized($string,$mode); >>
+
+Digests a string, skipping the initialization steps 
+of C<LaTeXML::digestString>
+Adds {document} wrapper in fragment mode.
+
+=back
+
+=head1 AUTHOR
+
+Deyan Ginev <d.ginev@jacobs-university.de>
+
+=head1 COPYRIGHT
+
+Public domain software, produced as part of work done by the
+United States Government & not subject to copyright in the US.
+
+=cut
