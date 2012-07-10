fadfa0a3bd75b2ec90f8b8f062bcafa65b39772e
deyan <deyan@2d0c7756-e317-0410-b71c-cbb6040a58f7>
2012-07-10 20:18:38 +0000

    updating test suite, merging, fixing runtime test, and adding the ids binding
    git-svn-id: https://svn.mathweb.org/repos/LaTeXML@2466 2d0c7756-e317-0410-b71c-cbb6040a58f7

--- branches/arXMLiv/lib/LaTeXML/Util/Extras.pm
+++ branches/arXMLiv/lib/LaTeXML/Util/Extras.pm
@@ -110,41 +110,6 @@ sub GetEmbeddable {
   return $embeddable||$doc;
 }
 
-our $id_xslt_dom = XML::LibXML->new()->parse_string(<<'EOT');
-<!-- this style sheet introduces automatic IDs to an XHTML document-->
-<xsl:stylesheet version="1.0"
-  xmlns:xsl = "http://www.w3.org/1999/XSL/Transform"
-  xmlns = "http://www.w3.org/1999/xhtml"
-  xmlns:xhtml = "http://www.w3.org/1999/xhtml"
-  xmlns:svg = "http://www.w3.org/2000/svg"
-  xmlns:m = "http://www.w3.org/1998/Math/MathML">
-
-<xsl:output method="xml" encoding="utf-8" omit-xml-declaration="no"
-  standalone="yes" doctype-public="-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN"
-  doctype-system="http://www.w3.org/TR/MathML2/dtd/xhtml-math11-f.dtd"
-  media-type     = 'application/xhtml+xml'
-  cdata-section-elements="data" indent="yes"/>
-  <xsl:template match="*">
-    <xsl:copy>
-      <xsl:if test="not(@xml:id) and not(@id)">
-        <xsl:attribute name="id">
-          <xsl:value-of select="generate-id(.)"/>
-        </xsl:attribute>
-      </xsl:if>
-      <xsl:copy-of select="@*"/>
-      <xsl:apply-templates/>
-   </xsl:copy>
-  </xsl:template>
-</xsl:stylesheet>
-EOT
-our $id_xslt = XML::LibXSLT->new()->parse_stylesheet($id_xslt_dom);
-
-sub InsertIDs {
-  my $doc = shift;
-  return unless defined $doc;
-  return LaTeXML::Post::Document->new($id_xslt->transform($doc->getDocument));
-}
-
 1;
 
 __END__
@@ -160,7 +125,6 @@ C<LaTeXML::Extras> - Extra goodies supporting LaTeXML's processing
     my $full_tex_doc = MathDoc($math_snippet);
     my $mathml_math_snippet = GetMath($xhtml_doc);
     my $body_div_snippet = GetEmbeddable($xhtml_doc);
-    my $xhtml_doc_with_ids = InsertIDs($xhtml_doc);
 
 =head1 DESCRIPTION
 
@@ -186,13 +150,6 @@ Extracts the first MathML math XML snippet in an XHTML document, provided as a
 
 Extracts the body <div> element of an XHTML document produced by LaTeXML's stylesheet for XHTML, 
     provided as a LaTeXML::Document object.
-
-=item C<< my $xhtml_doc_with_ids = InsertIDs($xhtml_doc); >>
-
-Inserts pseudo-random identifiers in any XHTML document, provided as a
-    LaTeXML::Document object on input.
-
-Uses XSLT's "generate-id()" mechanism.
 
 =back
 
